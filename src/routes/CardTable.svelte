ad12495b5a9cbc024d2f31d3582751802a0bfa3b
Cameron Chrobocinski <cameronchrobo@gmail.com>
2025-05-20 07:02:50 -0500

    added on hover conditional formatting for the room

--- src/routes/CardTable.svelte
+++ src/routes/CardTable.svelte
@@ -29,6 +29,9 @@
     let cardSize = $state({ width: 100, height: 145 });
     let room: Card[] = $state([]);
     let deckIsHovered = $state(false);
+    let monsterIsHovered = $state(0);
+    let weaponIsHovered = $state(0);
+    let potionIsHovered = $state(0);
 
     async function loadData() {
         const deck_response = await fetch(ASSETS_DECK + BYRON_KNOLL.replace(' ', '%20'));
@@ -78,60 +81,73 @@
 
 <div bind:this={cardTable} class="table-area">
     <!-- deck -->
-    <button
+    <div
         style="
-            width: {cardSize.width}px; 
-            height: {cardSize.height}px; 
-            object-fit: contain; 
-            margin: 6px;
-            background-color: {deckIsHovered ? (deck.length > 0 ? '#fff' : '#f00') : 'transparent'};
-            border: none;
+        width: {cardSize.width}px; 
+        height: {cardSize.height}px; 
+        object-fit: contain; 
+        margin: 6px;
+        background-color: {deckIsHovered
+            ? deck.length > 0
+                ? 'rgba(255, 255, 255, 0.2)'
+                : 'rgba(255, 0, 0, 0.2)'
+            : 'transparent'};
+        border: none;
         "
-        onclick={() => {
-            if (deck.length === 0) {
-                triggerError();
-            }
-            getRoom();
-        }}
-        onmouseenter={() => (deckIsHovered = true)}
-        onmouseleave={() => (deckIsHovered = false)}
     >
-        <div class="error-container">
-            {#each errorSymbols as id (id)}
-                <ErrorSymbol />
-            {/each}
-        </div>
-        {#if deck.length > 0}
-            <img
-                src={cardBack?.url}
-                alt="card back"
-                style="
+        <button
+            style="
+            width: 100%;
+            height: 100%; 
+            background-color: transparent;
+            border: none;
+            "
+            onclick={() => {
+                if (deck.length === 0) {
+                    triggerError();
+                }
+                getRoom();
+            }}
+            onmouseenter={() => (deckIsHovered = true)}
+            onmouseleave={() => (deckIsHovered = false)}
+        >
+            <div class="error-container">
+                {#each errorSymbols as id (id)}
+                    <ErrorSymbol />
+                {/each}
+            </div>
+            {#if deck.length > 0}
+                <img
+                    src={cardBack?.url}
+                    alt="card back"
+                    style="
                     width: 100%;
                     height: 100%;
-                "
-            />
-        {:else}
-            <div
-                style="
-                width: {cardSize.width}px;
-                height: {cardSize.height}px;
-                object-fit: contain;
-                margin: 6px;
-                background-color: #000;
-                opacity: 0.2;
-                "
-            ></div>
-        {/if}
-    </button>
+                    "
+                />
+            {:else}
+                <div
+                    style="
+                    width: {cardSize.width}px;
+                    height: {cardSize.height}px;
+                    object-fit: contain;
+                    margin: 6px;
+                    background-color: #000;
+                    opacity: 0.2;
+                    "
+                ></div>
+            {/if}
+        </button>
+    </div>
 
     <!-- buffer -->
     {#each [0, 1, 2] as _}
         <div
             style="
-                width: {cardSize.width}px; 
-                height: {cardSize.height}px; 
-                object-fit: contain; 
-                margin: 6px;
+            width: {cardSize.width}px; 
+            height: {cardSize.height}px; 
+            object-fit: contain; 
+            margin: 6px;
             "
         ></div>
     {/each}
@@ -139,17 +155,41 @@
     <!-- current room -->
     {#each [0, 1, 2, 3] as i}
         {#if room[i]}
-            <img
-                src={deckAssets.find((c) => c.suit === room[i]?.suit && c.value === room[i]?.value)
-                    ?.url}
-                alt="{VALUE_TO_RANK.get(deckAssets[i]?.value)} of {deckAssets[i]?.suit}"
+            <button
                 style="
                 width: {cardSize.width}px;
                 height: {cardSize.height}px;
                 object-fit: contain;
                 margin: 6px;
+                background-color: {(monsterIsHovered && monsterIsHovered - 1 === i) ||
+                (weaponIsHovered && weaponIsHovered - 1 === i) ||
+                (potionIsHovered && potionIsHovered - 1 === i)
+                    ? 'rgba(255, 255, 255, 0.2)'
+                    : 'transparent'};
+                border: none;
                 "
-            />
+            >
+                <img
+                    src={deckAssets.find(
+                        (c) => c.suit === room[i]?.suit && c.value === room[i]?.value,
+                    )?.url}
+                    alt="{VALUE_TO_RANK.get(deckAssets[i]?.value)} of {deckAssets[i]?.suit}"
+                    style="
+                        width: 100%;
+                        height: 100%;
+                    "
+                    onmouseenter={() => {
+                        monsterIsHovered = ['spades', 'clubs'].includes(room[i].suit) ? i + 1 : 0;
+                        weaponIsHovered = room[i].suit === 'diamonds' ? i + 1 : 0;
+                        potionIsHovered = room[i].suit === 'hearts' ? i + 1 : 0;
+                    }}
+                    onmouseleave={() => {
+                        monsterIsHovered = 0;
+                        weaponIsHovered = 0;
+                        potionIsHovered = 0;
+                    }}
+                /></button
+            >
         {:else}
             <div
                 style="
@@ -157,7 +197,7 @@
                 height: {cardSize.height}px;
                 object-fit: contain;
                 margin: 6px;
-            background-color: {deckIsHovered && deck[Math.max(i - deck.length, 0)]
+                background-color: {deckIsHovered && deck[Math.max(i - deck.length, 0)]
                     ? '#0f0'
                     : '#000'};
                 opacity: 0.2;
@@ -168,18 +208,29 @@
 
     <!-- health -->
     {#if die}
-        <img
-            src={die.find((d) => d.value === 20)?.url}
-            alt="die showing 20"
+        <div
             style="
             width: {cardSize.width}px;
             height: {cardSize.height}px;
             object-fit: contain;
             margin: 6px;
-        "
-        />
+            background-color: {monsterIsHovered
+                ? 'rgba(255, 0, 0, 0.2)'
+                : potionIsHovered
+                  ? 'rgba(0, 255, 0, 0.2)'
+                  : 'transparent'};
+            "
+        >
+            <img
+                src={die.find((d) => d.value === 20)?.url}
+                alt="die showing 20"
+                style="
+                width: 100%;
+                "
+            />
+        </div>
     {:else}
-        <!-- TODO: health -->
+        <!-- TODO: health logic -->
         <div
             style="
             width: {cardSize.width}px;
@@ -188,7 +239,7 @@
             margin: 6px;
             background-color: #000;
             opacity: 0.2;
-        "
+            "
         >
             20
         </div>
@@ -197,10 +248,11 @@
     <!-- weapon -->
     <div
         style="
-            width: {cardSize.width}px;
-            height: {cardSize.height}px;
-            object-fit: contain;
-            margin: 6px;
+        width: {cardSize.width}px;
+        height: {cardSize.height}px;
+        object-fit: contain;
+        margin: 6px;
+        background-color: {weaponIsHovered ? 'rgba(0, 255, 0, 0.2)' : 'rgba(0, 0, 0, 0.2)'};
         "
     >
         {#if weapon}
@@ -208,21 +260,10 @@
                 src={deckAssets.find((c) => c.value === weapon && c.suit === 'diamonds')?.url}
                 alt="{weapon} of diamonds"
                 style="
-                    width: 100%;
-                    height: 100%;
+                width: 100%;
+                height: 100%;
                 "
             />
-        {:else}
-            <div
-                style="
-                width: {cardSize.width}px;
-                height: {cardSize.height}px;
-                object-fit: contain;
-                margin: 6px;
-                background-color: #000;
-                opacity: 0.2;
-                "
-            ></div>
         {/if}
     </div>
 
@@ -230,10 +271,10 @@
     {#each [0, 1, 2, 3, 4, 5] as i}
         <div
             style="
-                width: {cardSize.width}px;
-                height: {cardSize.height}px;
-                object-fit: contain;
-                margin: 6px;
+            width: {cardSize.width}px;
+            height: {cardSize.height}px;
+            object-fit: contain;
+            margin: 6px;
             "
         >
             {#if enemies[i]}
@@ -242,17 +283,15 @@
                     style="
                     height: 100%; 
                     width: 100%;
-                    object-fit: contain;
-                    margin: 6px;
                     "
                 >
                     <img
                         src={deckAssets.find((c) => c.id === enemies[i]?.id)?.url}
                         alt="{VALUE_TO_RANK.get(enemies[i]?.value)} of {enemies[i]?.suit}"
                         style="
-                width: 100%;
-                height: 100%;
-            "
+                        width: 100%;
+                        height: 100%;
+                        "
                     />
                     <svg class="x-overlay" viewBox="0 0 100 100" preserveAspectRatio="none">
                         <line
@@ -278,13 +317,11 @@
             {:else}
                 <div
                     style="
-                width: {cardSize.width}px;
-                height: {cardSize.height}px;
-                object-fit: contain;
-                margin: 6px;
-                background-color: #000;
-                opacity: 0.2;
-                "
+                    width: {cardSize.width}px;
+                    height: {cardSize.height}px;
+                    background-color: #000;
+                    opacity: 0.2;
+                    "
                 ></div>
             {/if}
         </div>
